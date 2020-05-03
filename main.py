76f42635e54391e4805a9c9357e8d9db02c7db0f
Manuel Mosquera <mosqueramanuel5@gmail.com>
2020-05-03 19:55:16 +0000

    [Feature] The IP is stablished with cookies, therefore in the redirected route is taken the IP. modified:   main.py

--- main.py
+++ main.py
@@ -7,13 +7,15 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
+    user_ip = request.remote_addr
     response = make_response(redirect("/hello"))
+    response.set_cookie("user_ip", user_ip)
     return response
 
 
 @app.route("/hello")
 def hello():
-    user_ip = request.remote_addr
+    user_ip = request.cookies.get('user_ip')
     return "Hello world Platzi, this is your IP: {}".format(user_ip)
 
 if __name__ == "__main__":
