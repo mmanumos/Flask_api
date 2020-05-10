ed63b803423bddf7a603e0a7f38a1a6d6a060501
Manuel Mosquera <mosqueramanuel5@gmail.com>
2020-05-10 00:58:15 +0000

    [Feature] session is used to storage safe information like user_ip modified:   main.py

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 """ Flak app """
-from flask import Flask, request, make_response, redirect, render_template
+from flask import Flask, request, make_response, redirect
+from flask import render_template, session
 from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
+""" Config to generate a session with Flask """
+app.config['SECRET_KEY'] = 'SUPER SECRETO'
 
 todos = ["Levantarse temprano", "Hacer ejercicio", "Terminar tutorial"]
 
@@ -21,12 +24,12 @@ def not_found(error):
 def index():
     user_ip = request.remote_addr
     response = make_response(redirect("/hello"))
-    response.set_cookie("user_ip", user_ip)
+    session['user_ip'] = user_ip
     return response
 
 @app.route("/hello")
 def hello():
-    user_ip = request.cookies.get('user_ip')
+    user_ip = session.get('user_ip')
     context = {
         "user_ip" : user_ip,
         "todos" : todos
