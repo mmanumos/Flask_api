dc8c972f44b7891451ad2ffd48b0408f7f780f42
Manuel Mosquera <mosqueramanuel5@gmail.com>
2020-05-23 20:41:54 +0000

    [Feature] The new Blueprint auth is created. modified:   app/__init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,14 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
 from .config import Config
+from .auth import auth
 
 def create_app():
     app = Flask(__name__)
     bootstrap = Bootstrap(app)
     """ General configuration for app is setted by object Config"""
     app.config.from_object(Config)
+    """ General configuration for app is setted by object Config"""
+    app.register_blueprint(auth)
 
     return app
